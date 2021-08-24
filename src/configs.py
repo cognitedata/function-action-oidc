1ea067b511903502a00a07d5ce8a9cea5870709b
Håkon V. Treider <haakonvt@gmail.com>
2021-08-24 18:42:30 +0200

    More config-pydantic-mess

--- src/configs.py
+++ src/configs.py
@@ -1,11 +1,11 @@
 import logging
 from contextlib import suppress
-from functools import cached_property
 from inspect import signature
 from os import getenv
 from pathlib import Path
 from typing import Dict, List, Optional
 
+from cognite.client.exceptions import CogniteAPIError
 from pydantic import BaseModel, Field, constr, root_validator, validator
 from schedula import FunctionSchedule
 from yaml import safe_load
@@ -24,7 +24,7 @@ class GithubActionModel(BaseModel):
         """Magic parameter-load from env.vars. (Github Action Syntax)"""
 
         def get_parameter(val):
-            # GitHub action passes all missing arguments as an empty string:
+            # GitHub action passes all missing arguments as empty strings:
             return getenv(f"INPUT_{val.upper()}") or None
 
         return cls.parse_obj({k: get_parameter(k) for k in cls.schema()["properties"]})
@@ -38,16 +38,17 @@ class DeleteFunctionConfig(GithubActionModel):
         return self.remove_only
 
 
-class CredentialsModel:
+class CredentialsMixin:
     @property
     def credentials(self) -> Dict[str, str]:
         return self.dict(include={"client_id", "client_secret"})
 
+    @property
     def experimental_client(self):
         return create_oidc_client(**self.dict(by_alias=False, include=set(signature(create_oidc_client).parameters)))
 
 
-class DeployCredentials(GithubActionModel, CredentialsModel):
+class DeployCredentials(GithubActionModel, CredentialsMixin):
     cdf_project: Optional[NonEmptyString]
     cdf_cluster: NonEmptyString
     client_id: NonEmptyString = Field(alias="deployment_client_id")
@@ -55,48 +56,26 @@ class DeployCredentials(GithubActionModel, CredentialsModel):
     client_secret: NonEmptyString = Field(alias="deployment_client_secret")
 
 
-class SchedulesConfig(GithubActionModel, CredentialsModel):
-    schedule_file: constr(min_length=1, strip_whitespace=True, regex=r"^[\w\- /]+\.ya?ml$") = None  # noqa: F722
+class SchedulesConfig(GithubActionModel, CredentialsMixin):
+    schedule_file: Optional[constr(min_length=1, strip_whitespace=True, regex=r"^[\w\- /]+\.ya?ml$")]  # noqa: F722
     client_id: Optional[NonEmptyString] = Field(alias="schedules_client_id")
     client_secret: Optional[NonEmptyString] = Field(alias="schedules_client_secret")
     tenant_id: Optional[NonEmptyString] = Field(alias="schedules_tenant_id")
     cdf_cluster: Optional[NonEmptyString]
-
-    @cached_property
-    def schedules(self) -> List[FunctionSchedule]:
-        if self.schedule_file is None:
-            return []
-        path = self.function_folder / self.schedule_file
-        with path.open() as f:
-            return list(map(FunctionSchedule.parse_obj, safe_load(f)))
+    schedules: Optional[List[FunctionSchedule]]
 
     @root_validator(skip_on_failure=True)
-    def verify_schedules(cls, values):
-        if (schedule_file := values["schedule_file"]) is None:
-            return values
-        path = values["function_folder"] / schedule_file
-        if not path.is_file():
-            values["schedule_file"] = None
-            logger.warning(f"Ignoring given schedule file '{schedule_file}', path does not exist: {path.absolute()}")
-        return values
-
-    @root_validator(skip_on_failure=True)
-    def verify_schedule_credentials(cls, values):
+    def verify_schedule_credentials_are_given(cls, values):
         if values["schedule_file"] is None:
             return values
         # If schedules are given, credentials must be set:
-        c_secret, c_id = values["schedules_client_secret"], values["schedules_client_id"]
-        if c_secret is None or c_id is None:
+        c_secret, c_id, t_id = values["client_secret"], values["client_id"], values["tenant_id"]
+        if None in [c_secret, c_id, t_id]:
             raise ValueError(
-                "Schedules created for OIDC functions require additional client credentials (to be used "
-                "at runtime). Missing one or both of ['schedules_client_secret', 'schedules_client_id']"
+                "Schedules created for OIDC functions require additional client credentials (to be used  at runtime). "
+                "Missing one or more of ['schedules_client_secret', 'schedules_client_id', 'schedules_tenant_id']"
             )
-        verify_credentials(c_secret, c_id)
         return values
-
-
-def verify_credentials(client_secret: str, client_id: str) -> None:
-    pass  # TODO
 
 
 class FunctionConfig(GithubActionModel):
@@ -141,4 +120,42 @@ class FunctionConfig(GithubActionModel):
             # Try default directory 'common/':
             with suppress(ValueError):
                 values["common_folder"] = verify_path_is_directory(Path("common"))
+        return values
+
+
+class RunConfig(BaseModel):
+    schedules_config: SchedulesConfig
+    fn_config: FunctionConfig
+
+    @staticmethod
+    def verify_single_credential(cred_config, name):
+        try:
+            client = cred_config.experimental_client
+            res = client.iam.token.inspect()
+            # TODO: Is this really how to project??
+            logger.info(f"{name.title()} credentials verified towards {res.projects[0].url_name}!")
+        except CogniteAPIError:
+            raise ValueError(
+                f"{name.title()} credentials wrong or missing capabilities! (Test endpoint: /token/inspect)"
+            )
+
+    @root_validator(skip_on_failure=True)
+    def verify_credentials(cls, values):
+        cls.verify_single_credential(values["fn_config"], name="deploy")
+        if values["schedules_config"].schedule_file is None:
+            cls.verify_single_credential(values["schedules_config"], name="schedule")
+
+    @root_validator(skip_on_failure=True)
+    def verify_and_parse_schedules(cls, values) -> List[FunctionSchedule]:
+        if (schedule_file := values["schedules_config"].schedule_file) is None:
+            values["schedules_config"].schedules = []
+            return values
+
+        path = values["fn_config"].function_folder / schedule_file
+        if not path.is_file():
+            values["schedules_config"].schedule_file = None
+            logger.warning(f"Ignoring given schedule file '{schedule_file}', path does not exist: {path.absolute()}")
+        else:
+            with path.open() as f:
+                values["schedules_config"].schedules = list(map(FunctionSchedule.parse_obj, safe_load(f)))
         return values
--- src/index.py
+++ src/index.py
@@ -3,8 +3,8 @@ import logging
 from cognite.experimental import CogniteClient
 
 from checks import run_checks
-from configs import DeleteFunctionConfig, DeployCredentials, FunctionConfig, SchedulesConfig
-from function import remove_function_with_file, upload_and_create_function
+from configs import DeleteFunctionConfig, DeployCredentials, FunctionConfig, RunConfig, SchedulesConfig
+from orchestrator import remove_function_with_file, upload_and_create_function
 from schedule import deploy_schedules
 from setup_logging import configure_logging
 
@@ -14,18 +14,16 @@ logger = logging.getLogger(__name__)
 
 def main(
     client: CogniteClient,
-    fn_config: FunctionConfig,
-    deploy_creds: DeployCredentials,
-    schedule_config: SchedulesConfig,
+    config: RunConfig,
 ) -> None:
     # Run static analysis / other checks and pre-deployment verifications:
-    run_checks(fn_config)
+    run_checks(config.fn_config)
 
     # Deploy code to Cognite Functions:
-    fn = upload_and_create_function(client, fn_config)
+    fn = upload_and_create_function(client, config.fn_config)
 
     # Deploy schedules (if any):
-    deploy_schedules(client, fn, schedule_config)
+    deploy_schedules(client, fn, config.schedule_config)
 
     # Return output parameter (GitHub magic syntax):
     print(f"::set-output name=function_external_id::{fn.external_id}")
@@ -39,8 +37,9 @@ if __name__ == "__main__":
     if delete_config := DeleteFunctionConfig.from_envvars():
         remove_function_with_file(client, delete_config.function_external_id)
     else:
-        fn_config = FunctionConfig.from_envvars()
-        schedule_config = SchedulesConfig.from_envvars()
-
+        config = RunConfig(
+            fn_config=FunctionConfig.from_envvars(),
+            schedule_config=SchedulesConfig.from_envvars(),
+        )
         # Function Action OIDC, assemble!!
-        main(client, fn_config, deploy_creds, schedule_config)
+        main(client, config)
