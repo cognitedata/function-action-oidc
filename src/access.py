5c67cadb944b2c7ba079a521c413740689e78067
Håkon V. Treider <haakonvt@gmail.com>
2021-09-01 11:36:44 +0200

    Move all DatasetsAcl-related checks to config validation

--- src/access.py
+++ src/access.py
@@ -1,18 +1,21 @@
+from __future__ import annotations
+
 import logging
 from dataclasses import dataclass
-from typing import Dict, Iterable, Iterator, List
+from os import linesep
+from typing import Any, Dict, Iterable, Iterator, List
 
 from cognite.client.data_classes.iam import TokenInspection
 from cognite.client.exceptions import CogniteAPIError
+from cognite.experimental import CogniteClient
 
-from utils import create_oidc_client_from_dct
+from utils import retrieve_dataset
 
 logger = logging.getLogger(__name__)
 
 
-def verify_credentials_vs_project(creds: Dict[str, object], project: str, cred_name: str) -> TokenInspection:
+def verify_credentials_vs_project(client: CogniteClient, project: str, cred_name: str) -> TokenInspection:
     try:
-        client = create_oidc_client_from_dct(creds)
         token_inspect = client.iam.token.inspect()
     except CogniteAPIError:
         raise ValueError(
@@ -34,37 +37,81 @@ class Capability:
     acl: str
     version: int
     actions: List[str]
-    scope: Dict[str, object]
+    scope: Dict[str, Any]
     projects: List[str]
 
     @classmethod
-    def from_dict(cls, dct):
+    def from_dict(cls, dct: Dict[str, Any]) -> Capability:
         projects = dct.pop("projectScope")["projects"]
         (acl,) = dct  # magic voodoo syntax to extract the only key
         return cls(acl=acl, projects=projects, **dct[acl])
+
+    def is_all_scope(self) -> bool:
+        return "all" in self.scope
+
+    def is_dataset_scope(self, id: int) -> bool:
+        return id in self.scope.get("datasetScope", {}).get("ids", [])
+
+    def is_ids_scope(self, id: int) -> bool:
+        return id in self.scope.get("idScope", {}).get("ids", [])
 
 
 def filter_capabilities(capabilities: Iterable[Capability], acl: str) -> Iterator[Capability]:
     return filter(lambda c: c.acl == acl, capabilities)
 
 
-def missing_function_capabilities(capabilities: Iterable[Capability]):
+def missing_function_capabilities(capabilities: Iterable[Capability]) -> List[str]:
     actions = set(a for c in filter_capabilities(capabilities, acl="functionsAcl") for a in c.actions)
     if missing := set(["READ", "WRITE"]) - actions:
-        return [f"Functions:{m}" for m in missing]
+        return [f"FunctionsAcl:{m} (scope: 'all')" for m in missing]
     return []
 
 
-def missing_files_capabilities(capabilities: Iterable[Capability]):
-    # files_capes = list(filter_capabilities(capabilities, acl="filesAcl"))
-    # TODO: Implement!
-    logger.info("FilesAcl capabilities not verified! Reason: Not implemented yet ;)")
-    return []
+def missing_files_capabilities(
+    capabilities: Iterable[Capability], client: CogniteClient, ds_id: int = None
+) -> List[str]:
+    files_capes = list(filter_capabilities(capabilities, acl="filesAcl"))
+    files_actions_all_scope = set(a for c in files_capes for a in c.actions if c.is_all_scope())
+    missing_files_acl = set(["READ", "WRITE"]) - files_actions_all_scope
+
+    if ds_id is None:
+        # Not using a data set, so we require Files:READ/WRITE in scope=ALL:
+        if missing_files_acl:
+            return [f"FilesAcl:{m} (scope: 'all') (Tip: consider using a data set!)" for m in missing_files_acl]
+        return []
+
+    # If using a data set, we also accept *it* as scope for files:
+    missing_acls = []
+    files_actions_dsid_scope = set(a for c in files_capes for a in c.actions if c.is_dataset_scope(ds_id))
+    if missing_files_acl := missing_files_acl - files_actions_dsid_scope:
+        missing_acls += [f"FilesAcl:{m} (scope: 'all' OR 'dataset: {ds_id}')" for m in missing_files_acl]
+
+    data_set_capes = filter_capabilities(capabilities, acl="datasetsAcl")
+    data_set_actions = set(a for c in data_set_capes for a in c.actions if c.is_all_scope() or c.is_ids_scope(ds_id))
+    if "READ" not in data_set_actions:
+        # No read access to the given data set, so we can't check if it is write protected:
+        missing_acls.append(f"DatasetsAcl:READ (scope: 'all' OR 'id: {ds_id}')")
+        if "OWNER" not in data_set_actions:
+            missing_acls.append("(If dataset is write protected, you'll also need OWNER)")
+        return missing_acls
+
+    if retrieve_dataset(client, ds_id).write_protected:
+        if "OWNER" not in data_set_actions:
+            missing_acls.append(f"DatasetsAcl:OWNER (scope: 'all' OR 'id: {ds_id}'). NB: 'all scope' not recommended!")
+    return missing_acls
 
 
-def verify_capabilites(token_inspect: TokenInspection, project: str, cred_name: str) -> None:
+def verify_capabilites(
+    token_inspect: TokenInspection,
+    client: CogniteClient,
+    project: str,
+    ds_id: int = None,
+) -> None:
     capabilities = list(filter(lambda c: project in c.projects, map(Capability.from_dict, token_inspect.capabilities)))
-    missing = missing_function_capabilities(capabilities) + missing_files_capabilities(capabilities)
+    missing = missing_function_capabilities(capabilities) + missing_files_capabilities(capabilities, client, ds_id)
     if missing:
-        raise ValueError(f"{cred_name.title()} credentials missing one or more required capabilities: {missing}")
-    logger.info(f"{cred_name.title()} credentials capabilities verified!")
+        err_msg = f"Deploy credentials missing one or more required capabilities:\n{linesep.join(missing)}"
+        logger.error(err_msg)
+        raise ValueError(err_msg)
+
+    logger.info("Deploy credentials capabilities verified!")
--- src/function_file.py
+++ src/function_file.py
@@ -25,37 +25,20 @@ def _write_files_to_zip_buffer(zf: ZipFile, directory: Path):
 def upload_zipped_code_to_files(
     client: CogniteClient,
     file_bytes: bytes,
-    name: str,
+    xid: str,
     ds: DataSet,
 ) -> FileMetadata:
-    try:
-        return client.files.upload_bytes(
-            file_bytes,
-            name=name,
-            external_id=name,
-            data_set_id=ds.id,
-            overwrite=True,
-        )
-    except CogniteAPIError as exc:
-        if ds.id is None:
-            # Error is not dataset related, so we immediately re-raise
-            raise
-        if ds.write_protected:
-            err_msg = (
-                "Unable to upload file to WRITE-PROTECTED dataset: Deployment credentials MUST have capability "
-                "'dataset:OWNER' (and have 'files:WRITE' scoped to the same dataset OR all files)."
-            )
-        else:
-            err_msg = (
-                "Unable to upload file to dataset: Deployment credentials must have capability "
-                "'files:WRITE' scoped to the same dataset OR all files."
-            )
-        logger.error(err_msg)
-        raise CogniteAPIError(err_msg, exc.code, exc.x_request_id) from None
+    return client.files.upload_bytes(
+        file_bytes,
+        name=xid,
+        external_id=xid,
+        data_set_id=ds.id,
+        overwrite=True,
+    )
 
 
-def zip_and_upload_folder(client: CogniteClient, fn_config: FunctionConfig, name: str) -> int:
-    logger.info(f"Uploading code from '{fn_config.function_folder}' to '{name}'")
+def zip_and_upload_folder(client: CogniteClient, fn_config: FunctionConfig, xid: str) -> int:
+    logger.info(f"Uploading code from '{fn_config.function_folder}' to Files using external ID: '{xid}'")
     buf = io.BytesIO()  # TempDir, who needs that?! :rocket:
     with ZipFile(buf, mode="a") as zf:
         with temporary_chdir(fn_config.function_folder):
@@ -66,20 +49,21 @@ def zip_and_upload_folder(client: CogniteClient, fn_config: FunctionConfig, name
                 logger.info(f"- Added common directory: '{common_folder}' to the file/function")
                 _write_files_to_zip_buffer(zf, directory=common_folder)
 
-    if (ds_xid := fn_config.data_set_external_id) is not None:
-        ds = retrieve_dataset(client, ds_xid)
+    if (ds_id := fn_config.data_set_id) is not None:
+        ds = retrieve_dataset(client, ds_id)
         logger.info(
-            f"- Using dataset '{ds.external_id}' to govern the file (has write protection: {ds.write_protected})."
+            f"- Using dataset '{ds.external_id}' (ID: {ds_id}) to govern the file "
+            f"(has write protection: {ds.write_protected})."
         )
     else:
         ds = DataSet(id=None)
         logger.info("- No dataset will be used to govern the function zip-file!")
 
-    file_meta = upload_zipped_code_to_files(client, buf.getvalue(), name, ds)
+    file_meta = upload_zipped_code_to_files(client, buf.getvalue(), xid, ds)
     if (file_id := file_meta.id) is not None:
-        logger.info(f"- File uploaded successfully ({name})!")
+        logger.info(f"- File uploaded successfully ({xid})!")
         return file_id
-    raise FunctionDeployError(f"Failed to upload file ({name}) to CDF Files")
+    raise FunctionDeployError(f"Failed to upload file ({xid}) to CDF Files")
 
 
 def delete_function_file(client: CogniteClient, xid: str):
@@ -91,12 +75,8 @@ def delete_function_file(client: CogniteClient, xid: str):
     try:
         client.files.delete(external_id=xid)
         logger.info(f"- Delete of file '{xid}' successful!")
-    except CogniteAPIError:
-        if file_meta.data_set_id is None:
-            raise  # File is not protected by dataset, so we re-raise immediately
+    except CogniteAPIError as err:
         logger.error(
-            f"Unable to delete file! It is governed by data set with ID: {file_meta.data_set_id}. "
-            "Make sure your deployment credentials have write/owner access (see README.md in "
-            "function-action repo). Trying to ignore and continue as this workflow will "
+            f"Unable to delete file, reason: {err!r}! Trying to ignore and continue as this action will "
             "overwrite the file later."
         )
