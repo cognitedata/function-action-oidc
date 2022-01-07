6ba70da59aa1e94fff003def1082f48fc55bd6a2
Håkon V. Treider <haakonvt@gmail.com>
2022-01-07 16:24:12 +0100

    Update ast check to only accept root-level handle fns (#23)

--- src/checks.py
+++ src/checks.py
@@ -1,6 +1,7 @@
 import ast
 import logging
 from pathlib import Path
+from typing import List
 
 from configs import FunctionConfig
 from exceptions import FunctionValidationError
@@ -17,24 +18,44 @@ def run_checks(config: FunctionConfig) -> None:
         _check_handle_args(config.function_folder / config.function_file)
 
 
+class HandleVisitor(ast.NodeVisitor):
+    def __init__(self, file_path: Path):
+        self.file_path = file_path
+        self.handle_found = False
+        self.fn_names: List[str] = []
+
+    def visit_FunctionDef(self, fn_def):
+        """Overridden to get root function definitions only."""
+        self.fn_names.append(name := fn_def.name)  # The things we do for nice error messages
+        if name != "handle":
+            return
+        if self.handle_found:
+            logger.error(err_msg := "Multiple function definitions found!")
+            raise FunctionValidationError(err_msg)
+
+        bad_args = set(param.arg for param in fn_def.args.args).difference(HANDLE_ARGS)
+        if not bad_args:
+            logger.info(f"Signature of function entrypoint, 'handle', in file '{self.file_path}' validated!")
+            self.handle_found = True
+        else:
+            err_msg = (
+                f"In file '{self.file_path}', function 'handle' contained illegal args: {list(bad_args)}. "
+                f"The function args must be a subset of: {list(HANDLE_ARGS)} (ordering does NOT matter!)"
+            )
+            logger.error(err_msg)
+            raise FunctionValidationError(err_msg)
+
+
 def _check_handle_args(file_path: Path, fn_name: str = "handle") -> None:
     # If missing raises FileNotFoundError, which is a perfectly fine error message
     with file_path.open() as f:
         file = f.read()
 
-    for node in ast.walk(ast.parse(file)):
-        if isinstance(node, ast.FunctionDef) and node.name == fn_name:
-            bad_args = set(param.arg for param in node.args.args).difference(HANDLE_ARGS)
-            if not bad_args:
-                logger.info(f"Signature of function entrypoint, '{fn_name}', in file '{file_path}' validated!")
-                return
-            err_msg = (
-                f"In file '{file_path}', function '{fn_name}' contained illegal args: {list(bad_args)}. "
-                f"The function args must be a subset of: {list(HANDLE_ARGS)} (ordering is not important)"
-            )
-            logger.error(err_msg)
-            raise FunctionValidationError(err_msg)
-
-    err_msg = f"No function named '{fn_name}' was found in file '{file_path}'. It is required!"
-    logger.error(err_msg)
-    raise FunctionValidationError(err_msg)
+    (node_visitor := HandleVisitor(file_path)).visit(ast.parse(file))
+    if not node_visitor.handle_found:
+        err_msg = (
+            f"Required function named '{fn_name}' was not found in file '{file_path}' "
+            f"(functions found: {node_visitor.fn_names})."
+        )
+        logger.error(err_msg)
+        raise FunctionValidationError(err_msg)
