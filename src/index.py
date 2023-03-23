10fc143590dedfdb21a26efb12325e10e2dcb783
Håkon V. Treider <haakonvt@gmail.com>
2023-03-23 14:26:22 +0100

    dont write to GITHUB_OUTPUT in Azure...

--- src/index.py
+++ src/index.py
@@ -3,7 +3,15 @@ import os
 
 from checks import run_checks
 from cleanup import run_cleanup
-from configs import DeleteFunctionConfig, DeployCredentials, FunctionConfig, RunConfig, SchedulesConfig
+from configs import (
+    RUNNING_IN_AZURE_PIPE,
+    RUNNING_IN_GITHUB_ACTION,
+    DeleteFunctionConfig,
+    DeployCredentials,
+    FunctionConfig,
+    RunConfig,
+    SchedulesConfig,
+)
 from orchestrator import remove_function_with_file, upload_and_create_function
 from schedule import deploy_schedules
 from setup_logging import configure_logging
@@ -24,8 +32,11 @@ def main(config: RunConfig) -> None:
     run_cleanup(fn, config.function)
 
     # Return output parameter:
-    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
-        print(f"function_external_id={fn.external_id}", file=fh)
+    if RUNNING_IN_GITHUB_ACTION:
+        with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+            print(f"function_external_id={fn.external_id}", file=fh)
+    elif RUNNING_IN_AZURE_PIPE:  # can prob be removed (old github syntax)
+        print(f"::set-output name=function_external_id::{fn.external_id}")
 
 
 if __name__ == "__main__":
