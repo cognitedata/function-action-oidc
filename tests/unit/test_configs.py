0426864097367df6976788cab63714423b79acd4
Nina Odegard <nina.odegard@cognite.com>
2024-07-26 14:26:23 +0200

    feat: add tests for function_file validation to ensure posix valid

--- tests/unit/test_configs.py
+++ tests/unit/test_configs.py
@@ -88,3 +88,52 @@ class TestFunctionConfig:
 
         assert "metadata" in fn_params.keys()
         assert fn_params["metadata"] == metadata
+
+    def test_from_env_vars_function_file_default(self, gh_actions_env, monkeypatch, tmp_path):
+        from configs import FunctionConfig
+
+        test_func_xid = "test_func_123"
+        tmp_func_dir = tmp_path / test_func_xid
+        tmp_func_dir.mkdir()
+
+        monkeypatch.setenv("input_function_external_id".upper(), test_func_xid)
+        monkeypatch.setenv("input_function_folder".upper(), str(tmp_func_dir))
+
+        fc = FunctionConfig.from_envvars()
+
+        assert fc.function_external_id == test_func_xid
+        assert fc.function_folder == tmp_func_dir
+        assert fc.function_file == "handler.py"
+
+    @pytest.mark.parametrize("test_func_file", ["main.py", "foo/main.py", "foo/bar/main.py", "foo/bar/baz/main.py"])
+    def test_from_env_vars_function_file(self, gh_actions_env, monkeypatch, tmp_path, test_func_file):
+        from configs import FunctionConfig
+
+        test_func_xid = "test_func_123"
+        tmp_func_dir = tmp_path / test_func_xid
+        tmp_func_dir.mkdir()
+
+        monkeypatch.setenv("input_function_external_id".upper(), test_func_xid)
+        monkeypatch.setenv("input_function_folder".upper(), str(tmp_func_dir))
+        monkeypatch.setenv("input_function_file".upper(), test_func_file)
+
+        fc = FunctionConfig.from_envvars()
+
+        assert fc.function_external_id == test_func_xid
+        assert fc.function_folder == tmp_func_dir
+        assert fc.function_file == test_func_file
+
+    @pytest.mark.parametrize("test_func_file", [r"\handler.py", r"foo\handler.py", r"foo\\bar\handler.py"])
+    def test_from_env_vars_function_file_invalid_posix(self, gh_actions_env, monkeypatch, tmp_path, test_func_file):
+        from configs import FunctionConfig
+
+        test_func_xid = "test_func_123"
+        tmp_func_dir = tmp_path / test_func_xid
+        tmp_func_dir.mkdir()
+
+        monkeypatch.setenv("input_function_external_id".upper(), test_func_xid)
+        monkeypatch.setenv("input_function_folder".upper(), str(tmp_func_dir))
+        monkeypatch.setenv("input_function_file".upper(), test_func_file)
+
+        with pytest.raises(ValidationError):
+            FunctionConfig.from_envvars()
